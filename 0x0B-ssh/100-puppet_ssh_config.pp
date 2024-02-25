2942ac8009e77d01eb4d8fa79e284d1c140fe23c
eyramakua <eyramagginie@gmail.com>
2024-02-25 13:58:34 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,4 +5,11 @@ include stdlib
 file_line { 'Refuse to authenticate  using a password':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school'
+  line    => 'Password Authentication no',
+}
+
+file_line { 'Use private key':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
+}
